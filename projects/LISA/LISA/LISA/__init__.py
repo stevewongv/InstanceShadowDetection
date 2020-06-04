2c2e19e2fd1349c6fcc2e77f2540ef1d2b99c83b
Steve Wong <steve.w.git@icloud.com>
2020-06-04 10:12:47 +0800

    Update README

--- projects/LISA/LISA/LISA/__init__.py
+++
@@ -1,5 +0,0 @@
-#  Copyright (c) Tianyu Wang. All Rights Reserved.
-from .config import add_lisa_config
-from .LISA_meta_arch import LISARCNN
-from .LISA_rcnn import LISAROIHeads
-from .LISA_rpn import LISARPN
