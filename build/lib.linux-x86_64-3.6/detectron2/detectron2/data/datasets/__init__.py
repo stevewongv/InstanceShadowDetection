50764eb336f3194db382054fe537956dd8449c01
Steve Wong <steve.w.git@icloud.com>
2020-09-09 20:11:02 +0800

    Delete build files

--- build/lib.linux-x86_64-3.6/detectron2/detectron2/data/datasets/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-from .cityscapes import load_cityscapes_instances
-from .soba import load_soba_json, load_sem_seg
-from .lvis import load_lvis_json, register_lvis_instances, get_lvis_instances_meta
-from .register_coco import register_soba_instances
-from . import builtin  # ensure the builtin datasets are registered
-
-
-__all__ = [k for k in globals().keys() if "builtin" not in k and not k.startswith("_")]
